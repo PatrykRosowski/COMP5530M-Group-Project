685bfd94a158d1fed21aea0575b871eb07a5a105
Srinath S <83024778+Srinath-04@users.noreply.github.com>
2026-02-06 13:35:00 +0000

    Update XMLtoJSON.py flint check #11

--- Harrogate prototype/XMLtoJSON.py
+++ Harrogate prototype/XMLtoJSON.py
@@ -132,4 +132,3 @@ if tutorial == 1:
     > Data contains 1. Stop Point Reference (ATCO Code), 2. Common Name of bus stop, 3. Locality Name"
     )
     print("Run commands on terminal to access data")
-
