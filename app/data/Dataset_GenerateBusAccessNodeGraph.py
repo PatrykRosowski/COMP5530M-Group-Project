e93f70f593698b5fa701713e8dc47ef2b6890e50
Tom Joynson <sc21tj@leeds.ac.uk>
2026-04-19 10:27:10 +0100

    Added back the JSON extract, and added options to save as file or return as JSON.

--- app/data/Dataset_GenerateBusAccessNodeGraph.py
+++ app/data/Dataset_GenerateBusAccessNodeGraph.py
@@ -5,6 +5,8 @@ from app.data.GetAccessNodes import get_specific_stop_data
 import json
 import pickle
 
+### Constants ###
+SAVE_JSON = "ManchesterRoutes.json"
 
 ### --- File Extraction --- ###
 
@@ -48,7 +50,7 @@ def get_bus_access_node_graph(stop_json_filepath, route_json_filepath):
                 for prev_route in routesList[:-1]:
                     if routesList[-1] == prev_route:
                         routesList.remove(prev_route)
-                        
+
             first_file = False
             # break # Uncomment to only view 1st file bus routes
 
@@ -86,11 +88,91 @@ def get_bus_access_node_graph(stop_json_filepath, route_json_filepath):
 
 
 ### Main ###
+def get_bus_route_json(city, save=False, returnRoutes=True):
+    """
+    Generates a JSON of all pregenerated routes. Can save to a file and/or return the routes.
+
+    Args:
+        save (bool): If True, save to a file.
+        returnRoutes (bool): If True, return the JSON structure.
+
+    Returns:
+        routes (Dictionary):
+            [routeName : string,
+            route:
+                [ATCOCode : string,
+                Longitude : float,
+                Latitude : float]]
+    """
+
+    busStops = []
+    with open(f"app/data_files/Datasets/{city}/AllBusStopData.json", "r") as f:
+        fileData = json.load(f)
+
+        # Add each bus stop as a dictionary for cross join
+        for row in fileData:
+            for busStop in row:
+                # Assign each bus stop to JSON dictionary
+                busStopDict = {}
+                busStopDict["ATCOCode"] = busStop["StopPointRef"]
+                busStopDict["Latitude"] = busStop["Location"][1]
+
+                busStopDict["Longitude"] = busStop["Location"][0]
+
+                ## Assign bus stop to route
+                busStops.append(busStopDict)
+
+    # Create a lookup dictionary.
+    busStop_lookup = {stop["ATCOCode"]: stop for stop in busStops}
+
+    routes = []
+    route_signatures = set()  # Used to ensure there are no duplicates
+    with open(f"app/data_files/Datasets/{city}/AllRoutesData.json", "r") as f:
+        fileData = json.load(f)
+
+        # Add each route as a dictionary into routes array
+        i = 0
+        for route in fileData:
+            routeDict = {}
+            routeDict["RouteName"] = f"Pregenerated-{i}"
+            routeDict["Route"] = []
+            i += 1
+            for busStop in route:
+                # If ATCOCode already in route, break the loop as route is repeating.
+                if any(stop["ATCOCode"] == busStop["Start"] for stop in routeDict["Route"]):
+                    break
+
+                # Extract bus data from busStops array
+                extractedBusStop = busStop_lookup.get(busStop["Start"])
+                if extractedBusStop:
+                    routeDict["Route"].append(extractedBusStop)
+                else:
+                    pass
+
+            signature = tuple(stop["ATCOCode"] for stop in routeDict["Route"])
+
+            if signature not in route_signatures:
+                routes.append(routeDict)
+                route_signatures.add(signature)
+            else:
+                continue
+
+    # Save array of JSON routes as a JSON file
+    if save:
+        with open(SAVE_JSON, "w", encoding="utf-8") as f:
+            json.dump(routes, f)
+            print("Save Successful")
+
+    # Return array of JSON routes.
+    if returnRoutes:
+        return routes
+
 
 if __name__ == "__main__":
     import sys
 
-    sys.setrecursionlimit(100000)
-    graph = get_bus_access_node_graph("Manchester")
-    pickle.dump(graph, open("app/data_files/manchester_multimodal_graph.pkl", "wb"))
-    print("File succeccfully run!")
+    # sys.setrecursionlimit(100000)
+    # graph = get_bus_access_node_graph("Manchester")
+    # pickle.dump(graph, open("app/data_files/manchester_multimodal_graph.pkl", "wb"))
+    # print("File succeccfully run!")
+    print(get_bus_route_json("Manchester"))
